2ab27de8ce0086fd438850b10228c76e310ed718
atharvaProres <atharvadeshpande098@gmail.com>
2023-06-15 11:47:08 +0530

    Added the form for forgot password

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -8,7 +8,10 @@ export function userReducer(
   switch (action.type) {
     case "LOGIN":
       return action.payload;
-
+    case "VERIFY":
+      return { ...state, verified: action.payload };
+    case"LOGOUT":
+      return null;
     default:
       return state;
   }
